321050722f453854757965111ce95729b4ca05f6
ajay y <ajay.yvak@gmail.com>
2020-10-18 16:07:56 +0530

    commented the aws keys

--- s3bucket_tf/main.tf
+++ s3bucket_tf/main.tf
@@ -2,7 +2,7 @@
 provider "aws" {
   region        = "ap-south-1"
   # access_key    = "xxxxss"
-  # secret_key    = "xxssx"
+  # secret_key    = "xxssxsss"
 }
 
 resource "aws_s3_bucket" "terraform4" {
